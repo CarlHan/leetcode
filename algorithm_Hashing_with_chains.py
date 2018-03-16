d91611a52af57bce162f2c022d130f05b55b3c7f
CarlHan <hanlin_huang2017@163.com>
2018-03-16 21:02:02 +0800

    Update algorithm_Hashing_with_chains.py

--- algorithm_Hashing_with_chains.py
+++ algorithm_Hashing_with_chains.py
@@ -17,7 +17,14 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.hashtable = [[]] * bucket_count
+        self.hashtable = []
+        
+        for i in range(self.bucket_count):
+            self.hashtable.append([])
+        ##这个位置建表的时候可千万别用 self.hashtable = [[]] * self.bucket_count, 这样出现的问题是：
+        ##python此时并没有一个含有bucket_count数目的独立的[]，而只是在内存中指向同一个[]，所以导致的结果就是
+        ##如果想对其中一个比如hashtable[3].append(3)时，此时打印整个table会发现table中所有元素[]都append进去了一个3，因为它们
+        ##指向的都是同一个内存位置。所以需要用上面那个操作来保持各个元素[]之间的内存独立性！！！千万不要有所指……………………
 
     def _hash_func(self, s):
         ans = 0
@@ -39,46 +46,43 @@ class QueryProcessor:
             # use reverse order, because we append strings to the end
             #if self.hashtable[query.ind] == None:
             #    self.write_chain([])
-
-            self.write_chain(reversed(self.hashtable[query.ind]))
+            h_s = query.ind
+            chain = (self.hashtable[h_s])
+            print(' '.join(reversed(chain)))
         else:
-            if query.type == 'find':
+            if query.type == "find":
                 h_s = self._hash_func(query.s)
                 tmp = self.hashtable[h_s]
-                was_found = False
+                i = 0
                 if len(tmp) == 0:
-                    was_found = False
+                    print('no')
                 else:
                     for cur in range(len(tmp)):
                         if query.s == tmp[cur]:
-                            was_found = True
+                            i = 1
+                    if i == 0:
+                        print('no')
+                    else:
+                        print('yes')
                 #self.write_search_result(was_found)
-                if was_found:
-                    print('yes')
-                else:
-                    print('no')
 
-            elif query.type == 'add':
+            elif query.type == "add":
                     h_s = self._hash_func(query.s)
-                    tmp = self.hashtable[h_s]
-                    ind = False
-                    if len(tmp) == 0:
+                    i = 1
+                    if len(self.hashtable[h_s]) == 0:
                         (self.hashtable[h_s]).append(query.s)
                     else:
-                        for cur in range(len(tmp)):
-                            if query.s == tmp[cur]:
-                                ind = False
-                            else:
-                                ind = True
-                        if ind:
+                        for cur in range(len(self.hashtable[h_s])):
+                            if query.s == self.hashtable[h_s][cur]:
+                                i = 0
+                        if i == 1:
                             (self.hashtable[h_s]).append(query.s)
             else:
                 h_s = self._hash_func(query.s)
-                tmp = self.hashtable[h_s]
-                if len(tmp) != 0:
-                    for cur in tmp:
+                if len(self.hashtable[h_s]) != 0:
+                    for cur in self.hashtable[h_s]:
                         if query.s == cur:
-                            self.hashtable[h_s].pop(tmp.index(query.s))
+                            self.hashtable[h_s].pop(self.hashtable[h_s].index(query.s))
 
     def process_queries(self):
         n = int(input())
@@ -89,3 +93,4 @@ if __name__ == '__main__':
     bucket_count = int(input())
     proc = QueryProcessor(bucket_count)
     proc.process_queries()
+
